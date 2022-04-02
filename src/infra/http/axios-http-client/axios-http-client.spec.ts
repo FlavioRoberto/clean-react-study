91e11e306a8971b1d983d94e1269ebb46cc84b8d
Flávio Roberto Teixeira <flaviolive3@hotmail.com>
2022-04-02 13:14:14 -0300

    test: ensure AxiosHttpClient to response an HttpResponse on post

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -1,10 +1,15 @@
 import { AxiosHttpClient } from "./axios-http-client";
+import { HttpPostParams } from '@/data/protocols/http';
 import axios from 'axios';
 import faker from 'faker';
-import { HttpPostParams } from '@/data/protocols/http';
 
 jest.mock('axios')
 const mockedAxios = axios as jest.Mocked<typeof axios>;
+const mockedAxiosResult = {
+    data: faker.random.objectElement(),
+    status: faker.datatype.number()
+};
+mockedAxios.post.mockResolvedValue(mockedAxiosResult)
 
 const makeSut = (): AxiosHttpClient => {
     return new AxiosHttpClient();
@@ -20,5 +25,13 @@ describe('AxiousHttpClient', () => {
         const request = mockPostRequest();
         await makeSut().post(request);
         expect(mockedAxios.post).toHaveBeenCalledWith(request.url, request.body);
+    })
+
+    test('Should return the correct statusCode and body', async () => {
+        const response = await makeSut().post(mockPostRequest());
+        expect(response).toEqual({
+            statusCode: mockedAxiosResult.status,
+            body: mockedAxiosResult.data
+        })
     })
 })
